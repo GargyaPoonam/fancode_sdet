9ee6de6f7ad2d4378064f62e370b41318e0f7cf8
GargyaPoonam <poonams351@gmail.com>
2024-06-01 14:39:41 +0530

    Readme.md

--- src/test/java/Fancode/toDocheckerTest.java
+++ src/test/java/Fancode/toDocheckerTest.java
@@ -14,24 +14,24 @@ public class toDocheckerTest {
 	
 	@Test
     public void testFanCodeUsersTodosCompletion() {
-        // Base URI for the API
+     
         RestAssured.baseURI = "http://jsonplaceholder.typicode.com";
 
-        // Fetch users
+        // Fetch users data 
         Response usersResponse = given().get("/users");
         JsonPath usersJsonPath = usersResponse.jsonPath();
         List<Integer> userIds = usersJsonPath.getList("id");
         List<Double> userLatitudes = usersJsonPath.getList("address.geo.lat", Double.class);
         List<Double> userLongitudes = usersJsonPath.getList("address.geo.lng", Double.class);
 
-        // Iterate over users to find those in FanCode city
+        // for find users in city
         for (int i = 0; i < userIds.size(); i++) {
             double lat = userLatitudes.get(i);
             double lon = userLongitudes.get(i);
             if (lat > -40 && lat < 5 && lon > 5 && lon < 100) {
                 int userId = userIds.get(i);
                 
-                // Fetch todos for the user
+                // get todos for the user
                 Response todosResponse = given().param("userId", userId).get("/todos");
                 JsonPath todosJsonPath = todosResponse.jsonPath();
                 List<Boolean> todosCompleted = todosJsonPath.getList("completed");
@@ -39,7 +39,7 @@ public class toDocheckerTest {
                 long completedCount = todosCompleted.stream().filter(Boolean::booleanValue).count();
                 double completedPercentage = (double) completedCount / todosCompleted.size() * 100;
 
-                // Assert that the completed percentage is greater than 50%
+                // verify that the completed percentage is greater than 50%
                 assertTrue(completedPercentage > 50, "User ID: " + userId + " has less than 50% todos completed");
             }
         }
